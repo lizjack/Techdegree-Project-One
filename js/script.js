816c5d7077ddf104a2bc3bf7052fdd5b72e21a58
lizjack <65046262+lizjack@users.noreply.github.com>
2020-05-11 16:22:25 -0700

    Update Script.Js
    Added quotes

--- js/script.js
+++ js/script.js
@@ -10,8 +10,37 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
-
-
+let quotes = [
+          {
+            quote: 'Every minute a chance to change the world.'
+            source: 'Dolores Huerta'
+          },
+          {
+            quote: 'We are capable of building community anywhere.'
+            source: 'Austin Channing Brown'
+            citation: 'Im Still Here'
+            year: 2018
+          },
+          {
+            quote:'We are together. That makes this the perfect timeline.'
+            source: 'Abed Nadir'
+            citation: 'Community, Season 3: Episode 4'
+            year: 2011
+          },
+          {
+            quote: 'If I have learned anything, it is that I do not have to be limited by age or resources to make change and invigorate people.'
+            source: 'Nadya Okamoto'
+            citation: 'Feminist.com'
+            year: 2020
+          },
+          {
+            quote: 'I am streetlight, chillng in the heat. I illuminate the stores of the people in the street.'
+            source: 'Usnavi'
+            citation: 'In the Heights'
+            year: 2005
+          }
+];
+console.log(quotes);
 
 /***
  * `getRandomQuote` function
