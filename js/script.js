fb7219737385150d96adaa99fc3d6ff0b3469e48
lizjack <65046262+lizjack@users.noreply.github.com>
2020-05-12 14:33:53 -0700

    Fixed Indentation

--- js/script.js
+++ js/script.js
@@ -11,65 +11,65 @@ project 1 - A Random Quote Generator
  * Here I create `quotes` array filled with objects
 ***/
 let quotes = [
-          {
-            quote: 'The fact that you can build and you can make nonviolent change through organization; that\'s what I want my legacy to be.',
-            source: 'Dolores Huerta',
-            citation: 'aaro.org',
-            year: 2004
+      {
+        quote: 'The fact that you can build and you can make nonviolent change through organization; that\'s what I want my legacy to be.',
+        source: 'Dolores Huerta',
+        citation: 'aaro.org',
+        year: 2004
           },
           {
-            quote: 'We are capable of building community anywhere.',
-            source: 'Austin Channing Brown',
-            citation: 'I\'m Still Here',
-            year: 2018
-          },
-          {
-            quote:'We\'re together. That makes this the perfect timeline.',
-            source: 'Abed Nadir',
-            citation: 'Community, Season 3: Episode 4',
-            year: 2011,
-            tags: ', television'
-          },
-          {
-            quote: 'If I\'ve learned anything, it\'s that I do not have to be limited by age or resources to make change and invigorate people.',
-            source: 'Nadya Okamoto',
-            citation: 'Feminist.com',
-            year: 2020
-          },
-          {
-            quote: 'I\'m a streetlight, chillng in the heat. I illuminate the stories of the people in the street.',
-            source: 'Usnavi',
-            citation: 'In the Heights',
-            year: 2005
-          },
-          {
-            quote: 'Winter, spring, summer or fall, all you have to do is call and I\'ll be there. . .you\'ve got a friend.',
-            source: 'Carole King',
-            citation: 'Tapestry Album',
-            year: 1971,
-            tags: ', music'
-          },
-          {
-            quote: 'Freedom, freedom, where are you, cuz I need freedom too',
-            source: 'Beyonce',
-            citation: 'Homecoming',
-            year: 2019,
-            tags: ', music'
-          },
-          {
-            quote: 'People are just people. They shouldn\'t make you nervous. The world is everlasting -- it\'s coming and it\'s going.',
-            source: 'Regina Spektor',
-            citation: 'Soviet Kitsch Album',
-            year: 2003,
-            tags: ', music'
-          },
-          {
-           quote: 'Sometimes I think I lost something really important to me, and it turns out I already ate it.',
-           source: 'Troy Barnes',
-           citation: 'Community, Season 2: Episode 8',
-           year: 2010,
-           tags: ', television' 
-          }
+        quote: 'We are capable of building community anywhere.',
+        source: 'Austin Channing Brown',
+        citation: 'I\'m Still Here',
+        year: 2018
+      },
+      {
+        quote:'We\'re together. That makes this the perfect timeline.',
+        source: 'Abed Nadir',
+        citation: 'Community, Season 3: Episode 4',
+        year: 2011,
+        tags: ', television'
+      },
+      {
+        quote: 'If I\'ve learned anything, it\'s that I do not have to be limited by age or resources to make change and invigorate people.',
+        source: 'Nadya Okamoto',
+        citation: 'Feminist.com',
+        year: 2020
+      },
+      {
+        quote: 'I\'m a streetlight, chillng in the heat. I illuminate the stories of the people in the street.',
+        source: 'Usnavi',
+        citation: 'In the Heights',
+        year: 2005
+       },
+      {
+        quote: 'Winter, spring, summer or fall, all you have to do is call and I\'ll be there. . .you\'ve got a friend.',
+        source: 'Carole King',
+        citation: 'Tapestry Album',
+        year: 1971,
+        tags: ', music'
+      },
+      {
+        quote: 'Freedom, freedom, where are you, cuz I need freedom too',
+        source: 'Beyonce',
+        citation: 'Homecoming',
+        year: 2019,
+        tags: ', music'
+      },
+      {
+        quote: 'People are just people. They shouldn\'t make you nervous. The world is everlasting -- it\'s coming and it\'s going.',
+        source: 'Regina Spektor',
+        citation: 'Soviet Kitsch Album',
+        year: 2003,
+        tags: ', music'
+      },
+      {
+          quote: 'Sometimes I think I lost something really important to me, and it turns out I already ate it.',
+          source: 'Troy Barnes',
+          citation: 'Community, Season 2: Episode 8',
+          year: 2010,
+          tags: ', television' 
+      }
 ];
 
 /***
@@ -111,16 +111,16 @@ function printQuote(array) {
 window.setInterval(printQuote, 10000);
 
 /***Random background color function which is hoisted and used in printQuote()
-* I used and modified the following code from this resource:
+* I used and modified the following code from this
 *https://www.w3resource.com/javascript-exercises/javascript-math-exercise-40.php
 ***/
 function randomBg () {
-  let x = Math.floor(Math.random() * 256);
-  let y = Math.floor(Math.random() * 256);
-  let z = Math.floor(Math.random() * 256);
-  let randomBackground = "rgb(" + x + "," + y + "," + z + ")";
+      let x = Math.floor(Math.random() * 256);
+      let y = Math.floor(Math.random() * 256);
+      let z = Math.floor(Math.random() * 256);
+      let randomBackground = "rgb(" + x + "," + y + "," + z + ")";
 
-  return document.body.style.background = randomBackground;
+      return document.body.style.background = randomBackground;
 }
 
 
