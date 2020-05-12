41ba782acd46dfef8993c46170218162433c7838
lizjack <65046262+lizjack@users.noreply.github.com>
2020-05-12 10:42:02 -0700

    Added project comments
    Added comments within the code

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ project 1 - A Random Quote Generator
   // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
 /*** 
- * `quotes` array 
+ * Here I create `quotes` array filled with objects of quotes and additional information
 ***/
 let quotes = [
           {
@@ -27,7 +27,8 @@ let quotes = [
             quote:'We\'re together. That makes this the perfect timeline.',
             source: 'Abed Nadir',
             citation: 'Community, Season 3: Episode 4',
-            year: 2011
+            year: 2011,
+            tags: ', television'
           },
           {
             quote: 'If I\'ve learned anything, it\'s that I do not have to be limited by age or resources to make change and invigorate people.',
@@ -40,22 +41,53 @@ let quotes = [
             source: 'Usnavi',
             citation: 'In the Heights',
             year: 2005
+          },
+          {
+            quote: 'Winter, spring, summer or fall, all you have to do is call and I\'ll be there. . .you\'ve got a friend.',
+            source: 'Carole King',
+            citation: 'Tapestry Album',
+            year: 1971,
+            tags: ', music'
+          },
+          {
+            quote: 'Freedom, freedom, where are you, cuz I need freedom too',
+            source: 'Beyonce',
+            citation: 'Homecoming',
+            year: 2019,
+            tags: ', music'
+          },
+          {
+            quote: 'People are just people. They shouldn\'t make you nervous. The world is everlasting -- it\'s coming and it\'s going.',
+            source: 'Regina Spektor',
+            citation: 'Soviet Kitsch Album',
+            year: 2003,
+            tags: ', music'
+          },
+          {
+           quote: 'Sometimes I think I lost something really important to me, and it turns out I already ate it.',
+           source: 'Troy Barnes',
+           citation: 'Community, Season 2: Episode 8',
+           year: 2010,
+           tags: ', television' 
           }
 ];
 
 /***
- * `getRandomQuote` function
+ * Here I create the `getRandomQuote` function with math.floor() and math.random() to pick a random object within the quotes array
 ***/
 function getRandomQuote(array) {
          let randomNum = Math.floor(Math.random() * quotes.length);
-         return quotes[randomNum];        
-         // this isn't running in the function console.log(randomQuote);
+         return quotes[randomNum];       
 }
 getRandomQuote(quotes);
+//used console.log to test the function in the console
 console.log(getRandomQuote(quotes));
 
 /***
- * `printQuote` function
+ * Here I created the `printQuote` function using the getRandomQuote function. 
+ * Next, I concatendated the HTML strings and pulled the object's quote and source properties and used quoteHTML to store it.
+ * Next, I created if statements to include the additional object properties.
+ * Finally, I set the code snippet given to me from the project instructions equal to quoteHTML to update the HTML.
 ***/
 function printQuote(array) {
        let randomQuote = getRandomQuote(quotes);
@@ -67,6 +99,9 @@ function printQuote(array) {
       if (randomQuote.year) {
           quoteHTML += '<span class="year">' + randomQuote.year + '</span>';
       } 
+      if (randomQuote.tags) {
+        quoteHTML += '<span class="tags">' + randomQuote.tags + '</span>';
+      }
       quoteHTML += '</p>'
      return document.getElementById('quote-box').innerHTML = quoteHTML;
 } 
