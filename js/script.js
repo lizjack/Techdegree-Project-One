a9d24016cf7819860832518272659cab1e872daf
lizjack <65046262+lizjack@users.noreply.github.com>
2020-05-11 18:56:11 -0700

    Added getRandomQuote Function
    Added getRandomQuote Function

--- js/script.js
+++ js/script.js
@@ -12,46 +12,55 @@ project 1 - A Random Quote Generator
 ***/
 let quotes = [
           {
-            quote: 'Every minute a chance to change the world.'
-            source: 'Dolores Huerta'
+            quote: 'The fact that you can build and you can make nonviolent change through organization; that\'s what I want my legacy to be.',
+            source: 'Dolores Huerta',
+            citation: 'aaro.org',
+            year: 2004
           },
           {
-            quote: 'We are capable of building community anywhere.'
-            source: 'Austin Channing Brown'
-            citation: 'Im Still Here'
+            quote: 'We are capable of building community anywhere.',
+            source: 'Austin Channing Brown',
+            citation: 'Im Still Here',
             year: 2018
           },
           {
-            quote:'We are together. That makes this the perfect timeline.'
-            source: 'Abed Nadir'
-            citation: 'Community, Season 3: Episode 4'
+            quote:'We\'re together. That makes this the perfect timeline.',
+            source: 'Abed Nadir',
+            citation: 'Community, Season 3: Episode 4',
             year: 2011
           },
           {
-            quote: 'If I have learned anything, it is that I do not have to be limited by age or resources to make change and invigorate people.'
-            source: 'Nadya Okamoto'
-            citation: 'Feminist.com'
+            quote: 'If I\'ve learned anything, it\'s that I do not have to be limited by age or resources to make change and invigorate people.',
+            source: 'Nadya Okamoto',
+            citation: 'Feminist.com',
             year: 2020
           },
           {
-            quote: 'I am streetlight, chillng in the heat. I illuminate the stores of the people in the street.'
-            source: 'Usnavi'
-            citation: 'In the Heights'
+            quote: 'I\'m streetlight, chillng in the heat. I illuminate the stories of the people in the street.',
+            source: 'Usnavi',
+            citation: 'In the Heights',
             year: 2005
           }
 ];
-console.log(quotes);
 
 /***
  * `getRandomQuote` function
 ***/
-
+function getRandomQuote(array) {
+         let randomNum = Math.floor(Math.random() * quotes.length);
+         let randomQuote = quotes[randomNum];
+         return randomQuote;
+         console.log(randomQuote);
+}
+console.log(getRandomQuote(quotes));
 
 
 /***
  * `printQuote` function
 ***/
+function printQuote(array) {
 
+}
 
 
 /***
